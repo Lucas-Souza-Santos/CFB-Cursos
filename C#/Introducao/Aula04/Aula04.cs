74bd3a116becfc023daea2d0237426addb075065
Lucas-Souza-Santos <lucas210souza@gmail.com>
2020-12-04 23:53:39 -0300

    Aula 04 - escopo de variável

--- C#/Introducao/Aula04/Aula04.cs
+++ C#/Introducao/Aula04/Aula04.cs
@@ -1,13 +1,18 @@
 using System;
-
+/*
+* Aula 04 - Curso de Programação usando C#
+* Canal do Youtube: CFB-Cursos-Professor-Bruno
+* Assunto da Aula: Escopo de variável            
+*/ 
     class Aula04{
 
         static void Main(){
-            int num = 10;
-
-            Console.Write();
-                                   
-
+            int num = 0;   
+            
+        }
+        
+        void testa(){
+            Console.WriteLine(num);
         }
 }
 
