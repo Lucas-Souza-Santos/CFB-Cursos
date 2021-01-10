d17e8a4532b9d5478101a83d2776c784c6dd0309
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-09 23:26:45 -0300

    subindo tudo

--- C#/Introducao/Aula09/Aula09.cs
+++ C#/Introducao/Aula09/Aula09.cs
@@ -1,12 +1,9 @@
 using System;
-/*
-* Aula 04 - Curso de Programação usando C#
-* Canal do Youtube: CFB-Cursos-Professor-Bruno
-* Assunto da Aula: Escopo de variável            
-*/
+
 class Aula09{
     static void Main(){
-        
+
+        Console.WriteLine("Hello!!!");
+
     }
 }
-
